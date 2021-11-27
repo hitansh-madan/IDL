4d149601da7993f4fabfd6e59a6c43fefba40ffe
satyam verma <satyamverma603@gmail.com>
2021-11-27 22:51:01 +0530

    image added

--- src/components/fields/image.field.js
+++ src/components/fields/image.field.js
@@ -1,48 +1,65 @@
+import { getDownloadURL, ref, uploadBytesResumable } from "@firebase/storage";
 import { Component } from "react";
+import { storage } from "../../firebase";
 class ImageField extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        ...this.props.fieldObj,
-        data: this.props.fieldObj.data ? this.props.fieldObj.data : "",
-      };
-    }
-    
-    handleChange = (event) => {
-      this.setState(
-        {
-          data: event.target.value,
-        },
-        () => {
-          this.props.handleFieldChange(this.props.fieldTitle, this.state);
-        }
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...this.props.fieldObj,
+      data: this.props.fieldObj.data ? this.props.fieldObj.data : "",
     };
-  
-    render() {
-      return (
-        <div>
-          <label className="m-2">
-            <strong>{this.props.fieldTitle + " :"}</strong>
-          </label>
-          <p
-            type="button"
-            className="d-inline-block float-end m-2"
-            onClick={() => this.props.handleFieldDelete(this.props.fieldTitle)}
-          >
-            ❌
-          </p>
-          <div>
-            <input
-              type="file"
-              className="form-control"
-              value={this.state.data}
-              onChange={this.handleChange}
-            />
-          </div>
-        </div>
-      );
-    }
   }
+  formHandler = (e) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    console.log(file);
+    this.uploadFiles(file);
+  };
+  uploadFiles = (file) => {
+    if (!file) return;
+    const storageRef = ref(storage, '/files/' + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-  export default ImageField;
+    uploadTask.on("state_changed", (snapshot) => {
+    }
+      , (error) => {
+        console.log(error);
+      }
+      , () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+          console.log(url);
+          this.setState({ data: url });
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <label className="m-2">
+          <strong>{this.props.fieldTitle + " :"}</strong>
+        </label>
+        <p
+          type="button"
+          className="d-inline-block float-end m-2"
+          onClick={() => this.props.handleFieldDelete(this.props.fieldTitle)}
+        >
+          ❌
+        </p>
+        <div>
+          <input
+            type="file"
+            className="input"
+            value={this.state.data}
+            onChange={this.formHandler}
+          />
+        </div>
+        <div>
+          <button>submit</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ImageField;
